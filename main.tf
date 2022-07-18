43e1703ebfcc30a826731ad0cf5a9d9a2f6a8167
nemalipurianurag <nemalipurianurag@gmail.com>
2022-07-18 17:01:02 +0530

    added main.tf entries

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "google_project_service_identity" "privateca_sa" {
 
 # Create a KMS key-ring
 resource "google_kms_key_ring" "keyring" {
-  name     = "keyring-example12"
+  name     = "keyring-example33"
   location = "us-central1"
 }
 # # Create a KMS key within the provided KMS key-ring
@@ -97,7 +97,7 @@ resource "google_storage_bucket_iam_member" "cas_bucket_reader" {
 #Create a customer-managed bucket
 resource "google_storage_bucket" "cmek_bucket" {
   project       = "modular-scout-345114"
-  name          = "default212"
+  name          = "default233"
   force_destroy = true
   # encryption {
   #   default_kms_key_name = google_kms_crypto_key.secret.name
@@ -107,7 +107,7 @@ resource "google_storage_bucket" "cmek_bucket" {
 
 #creation of CA pool with teir as Enterprise
 resource "google_privateca_ca_pool" "example_ca_pool_enterprise" {
-  name     = "my-pool12"
+  name     = "my-pool33"
   location = "us-central1"
   tier     = "ENTERPRISE"
 
